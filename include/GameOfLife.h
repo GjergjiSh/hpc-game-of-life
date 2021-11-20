85bdcaee7a31270fd3e54680ecceefaadbbafd0d
GjergjiSh <gjergji.shkurti@steinbeis-ias.de>
2021-11-20 16:26:46 +0100

    Initial/unoptimized implementation of GoL

---
+++ include/GameOfLife.h
@@ -0,0 +1,39 @@
+/*
+
+Rules of the game:
+
+- Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+- Any live cell with two or three live neighbours lives on to the next generation.
+- Any live cell with more than three live neighbours dies, as if by overpopulation.
+- Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
+
+- Any live cell with two or three live neighbours survives.
+- Any dead cell with three live neighbours becomes a live cell.
+- All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <unistd.h>
+
+typedef struct
+{
+    std::vector<std::vector<int>> cell_rows;
+    int row_nr;
+    int col_nr;
+} board_t;
+
+
+void generate_initial_board_state(board_t& board, int rows, int cols);
+
+int cell_state_at(int row, int col, board_t& board);
+int get_neighbour_count(int row, int col, board_t& board);
+int generate_cell_state(int row, int col, board_t& board);
+void generate_next_board_state(board_t& board);
+
+void display_cell_state(int cell);
+void display_board_state(board_t& board);
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,25 @@
+#include "GameOfLife.h"
+#include "Patterns.h"
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-    std::string snake_case_hw = "Hello World";
-    std::cout << snake_case_hw << std::endl;
+
+    board_t board;
+
+    //board.col_nr = 20;
+    //board.row_nr = 10;
+    //board.cell_rows = GLIDER;
+
+    generate_initial_board_state(board, std::stoi(argv[1]), std::stoi(argv[2]));
+    int generations = std::stoi(argv[3]);
+
+    for (int gen = 0; gen < generations; gen++) {
+        std::cout << "State at generation: " << gen << std::endl;
+        display_board_state(board);
+        generate_next_board_state(board);
+
+    }
 
     return 0;
 }
